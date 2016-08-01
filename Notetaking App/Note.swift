a27f818c0d9945c37aeec57e474539f319821cc8
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-08-01 00:34:34 -0400

    Added Note struct

---
+++ Notetaking App/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  Notetaking App
+//
+//  Created by Gabriele on 8/1/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+struct Note {
+    var title: String
+    var content: String
+    var image: UIImage
+}
